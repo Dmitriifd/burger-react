1a799d78fcc568bdcf85f7bf0a0c99d55b48af09
Dmitriifd <idimka9@gmail.com>
2023-02-22 16:21:30 +0500

    count

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import style from "./Count.module.css";
 
 const Count = () => {
+  const [count, setCount] = useState(1);
+
+  const countIncrement = () => {
+    setCount(count + 1);
+  };
+  const countDecrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className={style.count}>
-      <button className={style.minus}>-</button>
-      <p className={style.amount}>1</p>
-      <button className={style.plus}>+</button>
+      <button
+        className={style.minus}
+        onClick={countDecrement}
+        disabled={count === 1}
+      >
+        -
+      </button>
+      <p className={style.amount}>{count}</p>
+      <button className={style.plus} onClick={countIncrement}>
+        +
+      </button>
     </div>
   );
 };
